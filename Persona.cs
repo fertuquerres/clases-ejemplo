d36c1c251f768336a1c3bc479a376a1582a48bd4
fertuquerres <56898229+fertuquerres@users.noreply.github.com>
2019-10-22 17:46:12 -0500

    Add files via upload

---
+++ Persona.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseConstructores
+{
+    public class Persona
+    {
+        private string nombre;
+        private int edad;
+        public Persona()
+        {
+            nombre = " ";
+            edad = 0;
+        }
+        public Persona(string nnombre, int nedad)
+        {
+            nombre = nnombre;
+            edad = nedad;
+        }
+        public void SetNombre(string nnombre)
+        {
+            nombre = nnombre;
+        }
+        public string GetNombre()
+        {
+            return nombre;
+        }
+        public void SetEdad(int nedad)
+        {
+            edad = nedad;
+        }
+        public int GetEdad()
+        {
+            return edad;
+        }
+        public void CalcularMayorEdad()
+        {
+            if (edad >= 18)
+            {
+                Console.WriteLine("Es mayor de edad");
+            }
+            else
+            {
+                Console.WriteLine("Es menor de edad");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseConstructores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Persona miPersona = new Persona();
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("Persona 0");
+            miPersona.CalcularMayorEdad();
+            Persona miPersona1 = new Persona("Fernando Tuquerres", 22);
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("Persona 1");
+            miPersona1.CalcularMayorEdad();
+            Persona miPersona2 = new Persona();
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("Persona 2");
+            Console.WriteLine("Ingrese el nombre de la persona: ");
+            miPersona2.SetNombre(Console.ReadLine());
+            Console.WriteLine("Ingrese la edad de la persona: ");
+            miPersona2.SetEdad(Convert.ToInt32(Console.ReadLine()));
+            miPersona2.CalcularMayorEdad();
+            Console.ReadKey();
+        }
+    }
+}
